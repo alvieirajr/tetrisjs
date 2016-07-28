d20b9a56434c5c2c82f7df21b9ac5ac6ad413ea2
alvieirajr <alvieirajr@gmail.com>
2016-07-28 13:14:45 -0300

    create contants.js and DetectCollisionOnTileInMovement()

---
+++ constants.js
@@ -0,0 +1,15 @@
+
+    const DIRECTION_TO_LEFT = 4;
+    const DIRECTION_TO_RIGHT = 6;
+    const DIRECTION_TO_BOTTOM = 2;
+    
+    const delta = 15;
+    const map_width_in_pieces = 15;
+    const map_height_in_pieces = 30;
+    const map_width_in_pixels = delta * map_width_in_pieces; 
+    const map_height_in_pixels = delta * map_height_in_pieces;  
+    
+    const canvas = document.querySelector("canvas");
+    const mapa = canvas.getContext("2d");
+    const tile = canvas.getContext("2d");
+    const colors = ['#27A4DD', '#9DD5C0', '#F39CC3', '#F1646C', '#FAC174'];    
