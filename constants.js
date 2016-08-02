821662c96305a8c1ac1b6fb54f5e5d935a13e914
alvieirajr <alvieirajr@gmail.com>
2016-08-01 21:08:45 -0300

    i forgot to commit the constants.js file

--- constants.js
+++ constants.js
@@ -8,8 +8,11 @@
     const map_height_in_pieces = 30;
     const map_width_in_pixels = delta * map_width_in_pieces; 
     const map_height_in_pixels = delta * map_height_in_pieces;  
+
     
     const canvas = document.querySelector("canvas");
     const mapa = canvas.getContext("2d");
     const tile = canvas.getContext("2d");
-    const colors = ['#27A4DD', '#9DD5C0', '#F39CC3', '#F1646C', '#FAC174'];    
+    const colors = ['#FFFFFF', '#27A4DD', '#9DD5C0', '#F39CC3', '#F1646C', '#FAC174']; // Cyclemon pallete
+    const background_collor = "#F5F3F2";
+    const pieces_type = ['tshape', 'line', 'box', 'lshape', 'slshape', 'srshape']; //
