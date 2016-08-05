b14216a249e5e7b3098305776cdd66a3da6fae89
alvieirajr <alvieirajr@gmail.com>
2016-08-05 07:05:10 -0300

    incomplete

--- constants.js
+++ constants.js
@@ -16,3 +16,4 @@
     const colors = ['#FFFFFF', '#27A4DD', '#9DD5C0', '#F39CC3', '#F1646C', '#FAC174']; // Cyclemon pallete
     const background_collor = "#F5F3F2";
     const pieces_type = ['tshape', 'line', 'box', 'lshape', 'slshape', 'srshape']; //
+    const pieces_type_angles = [4, 2, 1, 4, 2, 2]; //
